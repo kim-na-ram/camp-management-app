5c938f22a7c22dd52baebbbb1cbbfe4e9749f7ee
kim-na-ram <snrneh3@naver.com>
2024-08-01 15:33:45 +0900

    [Update] Grade 클래스에 등급 산출 메서드 추가
    - 등급 산출 메서드는 `type`에 따라 나뉜다
    - `score`를 받아 그에 맞는 Grade를 반환

--- src/main/java/com/bootcamp/utils/Grade.java
+++ src/main/java/com/bootcamp/utils/Grade.java
@@ -5,7 +5,24 @@ public enum Grade {
     B,
     C,
     D,
-    E,
     F,
-    N
+    N;
+
+    public static Grade mandatorySubjectGrade(int score) {
+        if(score >= 95) return Grade.A;
+        else if(score >= 90) return Grade.B;
+        else if(score >= 80) return Grade.C;
+        else if(score >= 70) return Grade.D;
+        else if(score >= 60) return Grade.F;
+        else return Grade.N;
+    }
+
+    public static Grade choiceSubjectGrade(int score) {
+        if(score >= 90) return Grade.A;
+        else if(score >= 80) return Grade.B;
+        else if(score >= 70) return Grade.C;
+        else if(score >= 60) return Grade.D;
+        else if(score >= 50) return Grade.F;
+        else return Grade.N;
+    }
 }
